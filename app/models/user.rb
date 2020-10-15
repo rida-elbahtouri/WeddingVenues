eac1e6dac15bf9145e3891feeafeb04846406c0e
rida-elbahtouri <elbahtouri0123@gmail.com>
2020-10-15 10:14:41 +0100

    create association between tables

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
+    validates :username,presence: true,uniqueness: true
+    has_many :favourites
+    has_many :venues ,through: :favourites,source: 'weddingvenue'
     
 end
--- app/models/weddingvenue.rb
+++ app/models/weddingvenue.rb
@@ -1,3 +1,8 @@
 class Weddingvenue < ApplicationRecord
-    
+    validates :name,presence: true
+    validates :description,presence: true
+    validates :location,presence: true
+    validates :price,presence: true
+
+    has_many :favourites
 end
