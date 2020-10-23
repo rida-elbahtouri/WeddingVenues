e5cfbce7115c77f3e024a386a28d83cc3956e5a6
rida-elbahtouri <elbahtouri0123@gmail.com>
2020-10-23 16:18:29 +0100

    fix rubocop issues

--- Gemfile
+++ Gemfile
@@ -24,7 +24,7 @@ gem 'jbuilder', '~> 2.7'
 gem 'hirb'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
-gem 'rack-cors',:require => 'rack/cors'
+gem 'rack-cors', require: 'rack/cors'
 
 # add secure password
 gem 'bcrypt', '~> 3.1', '>= 3.1.16'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,24 +3,22 @@ class UsersController < ApplicationController
     user = User.new(userparams)
     if user.save
       session[:user_id] = user.id
-            render json: {
-                status: :created,
-                loggen_in: true,
-                user: user
-            }
+      render json: {
+        status: :created,
+        loggen_in: true,
+        user: user
+      }
     else
-      render json:{
-        error:user.errors.full_messages
-    }
+      render json: {
+        error: user.errors.full_messages
+      }
     end
   end
 
-
-
-  
   private
+
   def userparams
     # require(:user).
-      params.permit(:username,:password,:password_confirmation)
+    params.permit(:username, :password, :password_confirmation)
   end
 end
